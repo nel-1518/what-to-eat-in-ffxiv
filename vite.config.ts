492ddadae709c66bebca44483635c1857fb31dcd
nel <neeeel@qq.com>
2026-07-12 19:27:28 +0800

    github workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/what-to-eat-in-ffxiv/',
   server: {
     host: true,
   },
